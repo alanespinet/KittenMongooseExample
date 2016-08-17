b58a749bc9abafdb8fbeb239dba5e0f08ab186db
alanespinet <aelluvetaa@gmail.com>
2016-08-16 18:23:44 -0700

    Example v2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // import mongoose
 const mongoose = require('mongoose');
+const express = require('express');
 
-// get necesary variables from mongose
+// get necesary variables from mongose and express
 const db = mongoose.connection;
 const Schema = mongoose.Schema;
+const app = express();
 
 // connect to the database 'kitten_tests'
 mongoose.connect('mongodb://localhost/kitten_tests');
@@ -27,6 +29,19 @@ kittySchema.methods.speak = function(){
 
 // compiling the Schema
 var Kitten = mongoose.model('Kitten', kittySchema);
+
+
+app.get("/", (req, res) => {
+  res.end('<h1>HELLO</h1>');
+})
+
+app.get("/find", (req, res) => {
+  Kitten.find()
+     .then( (records) => res.end(`<p>${records}</p>`))
+     .catch( (e) => console.log(e.message));
+});
+
+app.listen(9999, () => console.log("listening..."));
 
 // creating and testing the kitty
 // var pluto = new Kitten({name: 'Max'});
